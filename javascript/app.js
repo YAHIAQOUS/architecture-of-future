7c099d2240c7e573f07a66a9b6cb13a63c5e8df1
Yahia Qous <yahiaqous@gmail.com>
2021-03-18 14:44:19 +0200

    First use of JavaScripts

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+alert('Welcome to my first site, hope you like it');
+
+var interest= prompt("Are you interested to know about architecture of the future?");
+
+if (interest=='yes'){alert("Welcome to my site");}
+
+else{alert("You will be interested")};
+
+
